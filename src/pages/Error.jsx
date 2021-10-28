47d74dca0093acdeac8e237c0e552e9a9032e52b
Artem <artkola122@gmail.com>
2021-10-28 14:38:45 +0300

    fix error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import BackToEvent from '../Components/BackEvent/BackEvent';
 
 function ErrorPage() {
   return(
-    <h1 style={{color:'red'}}> Opppppss something go wrong</h1>
+    <>
+      <h1 style={{color:'red', marginBottom: '50px'}}> Opppppss something go wrong</h1>
+      <BackToEvent />
+    </>
   );
 }
 export default ErrorPage;
